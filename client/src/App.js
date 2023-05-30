df235ee4dce3da2e00ac160ddfc828a7ef394c2d
Adrian Ho <Adrianfpho@gmail.com>
2023-05-30 16:32:10 -0400

    Connect backend with frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function App(){
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return(
     <div>
-      
+      {backendData['info'][1]['score']['value']}
     </div>
   )
 }
