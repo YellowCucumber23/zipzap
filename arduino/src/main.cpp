995d238265fade4f90cc1deba9df532d27b3665a
Adrian Ho <Adrianfpho@gmail.com>
2023-06-06 19:38:16 -0400

    Arduino stuff

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -3,17 +3,25 @@
 // put function declarations here:
 int myFunction(int, int);
 
+int pot_pin = A0;
+int percent = 0;
+int prev_percent = 0;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(2000);
+  percent = round(analogRead(pot_pin) / 1024.0 * 100);
+
+  if(prev_percent != percent){
+    Serial.println(percent);
+    prev_percent = percent;
+  }
+  delay(100);
+  
 }
 
 // put function definitions here:
