41c48918943cf44e1e41cd3b4b17909ceb58a2f0
Adrian Ho <Adrianfpho@gmail.com>
2023-06-08 11:40:40 -0400

    Front Server Connection finished, need to add CSS to front end

--- client/src/components/ChessClock.js
+++ client/src/components/ChessClock.js
@@ -5,25 +5,76 @@ import {Chessboard} from "react-chessboard"
 function ChessClock({socket}){
     const [side, setSide] = useState("Start")
     const [render, renderSide] = useState(0)
-    const [fen, setFen] = useState("");
+    const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -");
+    const [score, setScore] = useState(0)
+
+    const [blackTime, setBlackTime] = useState(600)
+    const [whiteTime, setWhiteTime] = useState(600)
+
+    function setTime(time){
+        let minutes = Math.floor(time / 60)
+        let seconds = time % 60
+
+        if (seconds < 10) {seconds = "0"+seconds;}
+        return minutes + ':' + seconds
+    }
 
     useEffect(() => {
         socket.on('recieve_side', (data) => {
           setSide(data);
-          console.log("Data from recieve_side: ", side)
+          console.log("Data from recieve_side: ", data)
         });
+
+        socket.on('recieve_fen', (data) => {
+            setFen(data)
+            console.log(data)
+        })
+
+        socket.on('recieve_score', (data) => {
+            setScore(data)
+            console.log(data)
+        })
       }, [socket]);
+
+    useEffect(() => {
+        let interval;
+
+        if (side === 'White') {
+          interval = setInterval(() => {
+            setWhiteTime((prevTime) => {
+                if(prevTime > 0){
+                    return prevTime - 1
+                } else {
+                    return prevTime
+                }
+            });
+          }, 1000);
+        } else if(side === 'Black'){
+            interval = setInterval(() => {
+                setBlackTime((prevTime) => {
+                    if(prevTime > 0){
+                        return prevTime - 1
+                    } else {
+                        return prevTime
+                    }
+                });
+              }, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [side])
 
     return(
         <div>
             <div className= "board-container">
-                <Chessboard/>
+                <Chessboard position={fen}/>
             </div>
             <div className="container">
                 <div className="black-timer">
                     <div className="black-timer-info">
-                        <h1>Black time</h1>
+                        <h1>{setTime(blackTime)}</h1>
                         <button onClick={() => {
+                            socket.emit("send_side")
+                            socket.emit("send_fen")
                             socket.emit("send_side")
                             renderSide(render+1)
                             }}>
@@ -36,14 +87,19 @@ function ChessClock({socket}){
                 <div className="header-container">
                     <div className="header-info">
                         <h1>Current Turn: {side}</h1>
-                        <h1>Score: +0.34</h1>
+                        <h1>Score: {score}</h1>
                     </div>
                 </div>
 
                 <div className= "white-timer">
                         <div className="white-timer-info">
-                        <h1>White time</h1>
-                        <button onClick={() => socket.emit("send_side")}>
+                        <h1>{setTime(whiteTime)}</h1>
+                        <button onClick={() => {
+                            socket.emit("send_side")
+                            socket.emit("send_fen")
+                            socket.emit("send_score")
+                            renderSide(render+1)
+                            }}>
                         White Timer
                         </button>
                     </div>
--- server/server.js
+++ server/server.js
@@ -14,8 +14,13 @@ const io = new Server(server,{
     },
 })
 
+//Global Stuff
+var frontEndData = {};
+var prevScore = 0;
+
 //Send data to server
 const startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq e3 0 1"
+const fen1 = "rnbqkbnr/p1p2ppp/4p3/1p1p4/4P3/2NB4/PPPP1PPP/R1BQK1NR w KQkq -"
 const { Engine } = pkg;
 const enginePath = "/opt/homebrew/bin/stockfish"
 const engine = new Engine(enginePath)
@@ -23,29 +28,42 @@ await engine.init()
 await engine.setoption('MultiPV', '10')
 await engine.ucinewgame()
 
-var frontEndData = {};
-
 function getSide(currentFen){
     for(var i = 0; i < currentFen.length; i++){
         if(currentFen.charAt(i) == " "){
             if(currentFen.charAt(i+1) === "b"){
-                return "Black"
+                return "Black";
             } else if(currentFen.charAt(i+1) === "w"){
-                return "White"
+                return "White";
             }
         }
     }
 }
 
+function getScore(engineInfo, side){
 
-async function getScore(currentFen){
+    let scoreArray = [];
+    for(var i  = 1; i < engineInfo['info'].length; i++){
+        scoreArray.push(engineInfo['info'][i]['score']['value'])
+    }
+    scoreArray = scoreArray.sort(function(a, b){return a-b})
+
+    if(side === 'White'){
+        return scoreArray[scoreArray.length - 1] / 100.0
+    } else {
+        return scoreArray[0]/100.0
+    }
+}
+
+
+async function getFrontEndData(currentFen){
     await engine.isready()
     engine.position(currentFen)
     const result = await engine.go({depth: 3})
-
     frontEndData['move'] = result['bestmove']
     frontEndData['FEN'] = currentFen
     frontEndData['side'] = getSide(currentFen)
+    frontEndData['score'] = getScore(result, frontEndData['side'])
 
     return (frontEndData)
 }
@@ -55,15 +73,34 @@ io.on("connection", (socket) => {
     console.log('New client connected:', socket.id );
 
     socket.on("send_side",() => {
-        const promise = getScore(startFen)
-        // promise.then(result => {
-        //     socket.emit("recieve_side", result['side'])
-        // }).catch(error => {
-        //     console.error(error)
-        //     socket.emit("recieve_side", error.message)
-        // })
-        socket.emit("recieve_side", "black")
+        const promise = getFrontEndData(fen1)
+        promise.then(result => {
+            socket.emit("recieve_side", result['side'])
+        }).catch(error => {
+            console.error(error)
+            socket.emit("recieve_side", error.message)
+        })
     });
+
+    socket.on("send_fen",() => {
+        const promise = getFrontEndData(fen1)
+        promise.then(result => {
+            socket.emit("recieve_fen", result['FEN'])
+        }).catch(error => {
+            console.error(error)
+            socket.emit("recieve_fen", error.message)
+        })
+    });
+
+    socket.on("send_score", () => {
+        const promise = getFrontEndData(fen1)
+        promise.then(result => {
+            socket.emit("recieve_score", result['score'])
+        }).catch(error => {
+            console.error(error)
+            socket.emit("recieve_score", error.message)
+        })
+    })
 
     socket.on('disconnect', () => {
         console.log('Client disconnected.');
@@ -72,7 +109,7 @@ io.on("connection", (socket) => {
 
 
 server.listen(5000, () => {
-    const resultPromise = getScore(startFen);
+    const resultPromise = getFrontEndData(fen1);
 
     resultPromise.then(result => {
         console.log(result)
